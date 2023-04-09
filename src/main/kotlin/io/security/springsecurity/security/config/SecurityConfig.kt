a313578042549f499b9d39fdcea41ecd56137ce4
Hyune-c <choi8608@gmail.com>
2023-04-10 01:12:49 +0900

    [스프링 시큐리티 기본 API 및 Filter 이해] 6) Remember Me 인증 Hyune-c

--- src/main/kotlin/io/security/springsecurity/security/config/SecurityConfig.kt
+++ src/main/kotlin/io/security/springsecurity/security/config/SecurityConfig.kt
@@ -3,10 +3,11 @@ package io.security.springsecurity.security.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(val userDetailsService: UserDetailsService) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.authorizeRequests()
@@ -28,19 +29,26 @@ class SecurityConfig {
 //                println("### exception: ${exception.message}")
 //                response.sendRedirect("/login")
 //            }
+
+            // 5) logout
+//            .and()
+//            .logout()
+//            .logoutUrl("/logout")
+//            .logoutSuccessUrl("/login")
+//            .addLogoutHandler { request, _, _ ->
+//                println("### logout handler")
+//                request.session.invalidate()
+//            }
+//            .logoutSuccessHandler { _, response, _ ->
+//                println("### logout success handler")
+//                response.sendRedirect("/login")
+//            }
+//            .deleteCookies("remember-me")
             .and()
-            .logout()
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/login")
-            .addLogoutHandler { request, _, _ ->
-                println("### logout handler")
-                request.session.invalidate()
-            }
-            .logoutSuccessHandler { _, response, _ ->
-                println("### logout success handler")
-                response.sendRedirect("/login")
-            }
-            .deleteCookies("remember-me")
+            .rememberMe()
+            .rememberMeParameter("remember")
+            .tokenValiditySeconds(3600)
+            .userDetailsService(userDetailsService)
         return http.build()
     }
 }
